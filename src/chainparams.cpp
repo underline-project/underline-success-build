86d53f11eefe9d7afa8a47eef15bb99bafc28987
underline-project <xmrpoolmine@gmail.com>
2018-03-22 02:09:17 -0400

    step 5 hackster.io

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -111,10 +111,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
+        pchMessageStart[0] = 0x55; //HX "U" http://www.asciitable.com/
+        pchMessageStart[1] = 0x43; //HX "C" http://www.asciitable.com/
+        pchMessageStart[2] = 0x4f; //HX "O" http://www.asciitable.com/
+        pchMessageStart[3] = 0x49; //HX "I" http://www.asciitable.com/
         nDefaultPort = 9333;
         nPruneAfterHeight = 100000;
 
@@ -130,12 +130,12 @@ public:
         vSeeds.emplace_back("dnsseed.underlinepool.org", true);
         vSeeds.emplace_back("dnsseed.koin-project.com", false);
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,68); //Decimal version 'U"
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,50);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
-        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
-        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,68);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x75, 0x88, 0xB2, 0x1E}; //HX "u" http://www.asciitable.com/
+        base58Prefixes[EXT_SECRET_KEY] = {0x75, 0x88, 0xAD, 0xE4}; //HX "u" http://www.asciitable.com/
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
